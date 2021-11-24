70213afb1941293649c3669fa1eec393b4846a9b
Gabriel de Sousa <gabrieldesousa@gabriels-mbp.lan>
2021-11-24 11:06:05 -0800

    data

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,28 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+resp = RestClient::Request.execute(method: :get,
+  url: "https://api.spaceflightnewsapi.net/v3/articles",
+  headers: {
+    'Content-Type': 'application/json'
+  })
+
+  article_data = JSON.parse(resp.body)
+  articles = article_data
+  puts articles
+
+articles.each do |blog|
+  Article.create({
+    featured: blog["featured"],
+    title: blog["title"],
+    url: blog["url"],
+    imageUrl: blog["imageUrl"],
+    newsSite: blog["newsSite"],
+    summary: blog["summary"],
+    publishedAt: blog["publishedAt"],
+    launches: blog["launches"],
+    events: blog["events"]
+
+})
+end

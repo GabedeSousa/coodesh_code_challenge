c909eb9297b8cdfdae6f1f5e21accafd7e975c01
Gabriel de Sousa <gabrieldesousa@gabriels-mbp.lan>
2021-11-26 17:38:07 -0800

    fixed CSRF

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,4 +1,5 @@
 class ApplicationController < ActionController::Base
+  skip_before_action :verify_authenticity_token
 
   def render_error(message: nil, fields: nil, status: :unprocessable_entity)
     errors = {}
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,5 +1,5 @@
 class ArticlesController < ApplicationController
-  before_action :set_article, only: [:show, :update]
+  before_action :set_article, only: [:show, :update, :destroy]
 
   def index
     @articles = Article.all

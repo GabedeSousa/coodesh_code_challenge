e42bf59f3d8fa24baba28e324fb78c94f1e645e2
Gabriel de Sousa <gabrieldesousa@gabriels-mbp.lan>
2021-11-28 09:45:36 -0800

    sidekiq

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,8 @@
+require 'sidekiq/web'
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   # root 'welcome#index'
+  mount Sidekiq::Web => '/sidekiq'
   scope '/api', defaults: {format: :json} do
     resources :articles, only: [:index, :show, :create, :update, :destroy]
   end
